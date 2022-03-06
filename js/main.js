b69a58144ccba57df91bdf9bcbd6b8fd37842634
itzJunaedRayhan <webgenesis99@gmail.com>
2022-03-06 14:12:21 +0600

    Done

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let changeBG = document.getElementById("changeBG");
+
+
+//  Random Background color Change Function:
+function backgroundChage () {
+    let wrapper = document.querySelector(".wrapper");
+    wrapper.style.backgroundColor = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;  
+}
+
+
+changeBG.addEventListener("click", backgroundChage)
