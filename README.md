59474c48913ffbe5f6c8db11e024049ff1f7da43
itzJunaedRayhan <webgenesis99@gmail.com>
2022-03-06 14:15:20 +0600

    Done

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Random-Background-Color
+# Random-Background-Color-Change:
+
+Technology Used: Html, Css, JavaScript
+
+Live Link:
+
+GitHub Code: https://github.com/itzJunaedRayhan/Random-Background-Color
+
+Author: https://www.linkedin.com/in/itzjunaedrayhan/
--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 let changeBG = document.getElementById("changeBG");
 
-
 //  Random Background color Change Function:
 function backgroundChage () {
     let wrapper = document.querySelector(".wrapper");
     wrapper.style.backgroundColor = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;  
 }
 
-
+//  onclick event with addEventListener:
 changeBG.addEventListener("click", backgroundChage)
